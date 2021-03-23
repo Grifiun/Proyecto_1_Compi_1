1d227db35176e588f63ea699371be421b4100917
Grifiun <denii.grifon@gmail.com>
2021-03-23 01:49:53 -0600

    se agrego un constructor al Http cliente y se quito una libreria que se habia implementado antes

--- Proyecto_de_prueba_P1_Compi_1/src/main/java/http/Cliente.java
+++ Proyecto_de_prueba_P1_Compi_1/src/main/java/http/Cliente.java
@@ -11,54 +11,25 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
-/*
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-*/
-
-
 /**
  *
  * @author grifiun
  */
 public class Cliente {
-    /*
-    private HttpClient httpCliente;
-    private HttpGet get;
-    private HttpResponse response;
-    private String resource;//respuesta del server
     
-    public Cliente(){
-        this.httpCliente = HttpClients.createDefault();
-        this.get = null;
-        this.resource = "";
-    }
-    
-    public String GET(String url){
-        this.get = new HttpGet(url);
-               
-        try {
-            this.response = this.httpCliente.execute(this.get);//realizamos la peticion
-            this.resource = EntityUtils.toString(this.response.getEntity());
-        } catch (Exception ex) {
-            System.out.println("Error en la conexion con el servidor: "+ex.getMessage());
-        }        
-        
-        //respuesta
-        return this.resource;
-    }
-    */
     private HttpClient httpCliente;
     private HttpRequest request;
-    private String resource = "";//respuesta del server
+    private String resource;//respuesta del server
     
-        
+    
+    public Cliente(){
+        this.httpCliente = HttpClient.newHttpClient();
+        this.request = null;
+        this.resource = "";
+    }
+            
     public String POST(String url, String texto) throws Exception{
-        httpCliente = HttpClient.newHttpClient();
+        //httpCliente = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
           .POST(HttpRequest.BodyPublishers.ofString(texto))
           .uri(URI.create(url))
